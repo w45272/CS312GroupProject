d32689746b8a9b797e5a9dea65fa78dbba3da8e8
w45272 <w45272@rams.colostate.edu>
2022-03-26 15:13:19 -0600

    A B B B B C add

--- fuel/app_project/views/m1/about.php
+++ fuel/app_project/views/m1/about.php
@@ -1,41 +1,42 @@
 <?php echo Asset::css('about.css'); ?>
 
-<div class="about"> 
-    
-    <div class="members">
-        Ethan Seefried
-        <hr>
-        <?php echo Asset::img('eseefried_profile.jpeg'); ?>
-        <hr>
-        <p>"I'm a senior studying Phyics and Computer Science at CSU. In my free time I enjoy anime, video games,
-            and playing sports! I'm also currently conducting research in the CSU vision lab to help detect methane emissions."</p>
+<div class="about">
+
+  <div class="members">
+    Ethan Seefried
+    <hr>
+    <?php echo Asset::img('eseefried_profile.jpeg'); ?>
+    <hr>
+    <p>"I'm a senior studying Phyics and Computer Science at CSU. In my free time I enjoy anime, video games,
+      and playing sports! I'm also currently conducting research in the CSU vision lab to help detect methane emissions."</p>
+  </div>
+  <div class="members">
+    Will Mingus
+    <hr>
+    <?php echo Asset::img('eseefried_profile.jpeg'); ?>
+    <hr>
+    <p>"I am an Applied Physics major concentrating in computer science. Currently, I work in Colorado State
+      University's High Energy Physics lab simulating particles traveling through liquid argon. In my free time
+      I enjoy being outdoors and spending time with friends and family."</p>
+  </div>
+  <div class="members">
+    Mariah Bradford
+    <hr>
+    <?php echo Asset::img('eseefried_profile.jpeg'); ?>
+    <hr>
+    <p>"I'm a senior studying Phyics and Computer Science at CSU. In my free time I enjoy anime, video games,
+      and playing sports! I'm also currently conducting research in the CSU vision lab to help detect methane emissions."</p>
+  </div>
+  <div class="members">
+    Joshua Keene
+    <hr>
+    <div>
+      <?php echo Asset::img('backlira_profile.jpg'); ?>
     </div>
-    <div class="members">
-        Will Mingus
-        <hr>
-        <?php echo Asset::img('eseefried_profile.jpeg'); ?>
-        <hr>
-        <p>"I'm a senior studying Phyics and Computer Science at CSU. In my free time I enjoy anime, video games,
-            and playing sports! I'm also currently conducting research in the CSU vision lab to help detect methane emissions."</p>
-    </div>
-    <div class="members">
-        Mariah Bradford
-        <hr>
-        <?php echo Asset::img('eseefried_profile.jpeg'); ?>
-        <hr>
-        <p>"I'm a senior studying Phyics and Computer Science at CSU. In my free time I enjoy anime, video games,
-            and playing sports! I'm also currently conducting research in the CSU vision lab to help detect methane emissions."</p>
-    </div>
-    <div class="members">
-        Joshua Keene
-        <hr>
-        <div>
-        <?php echo Asset::img('backlira_profile.jpg'); ?>
-        </div>
-        <hr>
-        <p>"Currently studying Computer Science at Colorado State University. I live in here in Ft. Collins and
-         I work on campus for the ACNS Telecom department. In my spare time, I enjoy riding (05' Suzuki Boulevard)
-          and gaming."</p>
-    </div>
-    
+    <hr>
+    <p>"Currently studying Computer Science at Colorado State University. I live in here in Ft. Collins and
+      I work on campus for the ACNS Telecom department. In my spare time, I enjoy riding (05' Suzuki Boulevard)
+      and gaming."</p>
+  </div>
+
 </div>
