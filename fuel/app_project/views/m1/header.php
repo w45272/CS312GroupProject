ac5c194ffaf8ec5f645bd076ac8cce9a2f5f3c7c
Ethan Seefried <eseefrie@colostate.edu>
2022-03-26 11:51:43 -0600

    added company name

--- fuel/app_project/views/m1/header.php
+++ fuel/app_project/views/m1/header.php
@@ -3,6 +3,7 @@
 <div class="header">
     <hr>
         <?php echo Asset::img('logo.png',array('id'=>'logo')); ?>
+        <h2> Calico </h2>
         <h1><?php echo $title; ?></h1>
     <hr>
 </div>      
