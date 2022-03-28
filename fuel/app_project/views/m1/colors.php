1883fffd41532ee64e238063772f09bea12acb5b
backlira <backlira@rams.colostate.edu>
2022-03-28 12:55:58 -0600

    add dynamic tables and styling.

--- fuel/app_project/views/m1/colors.php
+++ fuel/app_project/views/m1/colors.php
@@ -2,19 +2,45 @@
 
 <div class="colors">
     <table id="small_table">
+        <col class="small_col_one">
+        <col class="small_col_two">
+        <thead>
+        </thead>
         <?php
         for($x=0; $x<$count; $x++){
-            echo "<tr><td>color</td><td>color</td></tr>";
+            echo "<tr><td><select id='color'".$x." name='colors'> 
+                            <option value='red'>Red</option>
+                            <option value='orange'>Orange</option>
+                            <option value='yellow'>Yellow</option>
+                            <option value='green'>Green</option>
+                            <option value='blue'>Blue</option>
+                            <option value='purple'>Purple</option>
+                            <option value='grey'>Grey</option>
+                            <option value='brown'>Brown</option>
+                            <option value='black'>Black</option>
+                            <option value='teal'>Teal</option>
+                           </select> 
+                    </td>
+                    <td>color</td>
+                  </tr>";
         }
         ?>
-    
+        
     </table>
+    
+    <p></p>
+    <p></p>
+    <p></p>
+    
     <table id="large_table">
         <?php
         for($row=0; $row<$size+1; $row++){
             echo "<tr>";
-            for($col=0; $col<$size+1; $col++){
-                echo "<td>.$row.$col.</td>";
+            for($col=0, $letter='A'; $col<$size+1; $col++){
+                if($row==0 && $col==0){echo "<td> </td>";}
+                elseif($row==0){echo "<td> $letter </td>"; $letter++;}
+                elseif($col==0){echo "<td> $row </td>";}
+                else{echo "<td> </td>";}
             }
             echo "</tr>";
         }
@@ -23,9 +49,8 @@
     </table>
     
     <?php
-        echo "<p>Selected a ".$size." by ".$size." grid!<p>";
-        echo "<p>Selected a Palette of ".$count." Colors!<p>";
+        echo "<button id='print_button'>Print Page</button>";
     ?>
 
-    Color Coordinator dummy page....
+    
 </div>
