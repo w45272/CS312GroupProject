0ba7641d3a7d087c569dbb2825824a172336f600
Ethan Seefried <eseefrie@colostate.edu>
2022-03-30 20:59:07 -0600

    Update footer.php

--- fuel/app_project/views/m1/footer.php
+++ fuel/app_project/views/m1/footer.php
@@ -1,5 +1,5 @@
 <?php echo Asset::css('footer.css'); ?>
 
 <div class="footer">
-    Contact Information: w45272@rams.colostate.edu
+    Contact Information: w45272@rams.colostate.edu, eseefrie@rams.colostate.edu, backlira@rams.colostate.edu, mbrad@rams.colostate.edu
 </div>
