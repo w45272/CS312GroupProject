f18befd10de801883d5a6897a430581637d9d889
w45272 <85902196+w45272@users.noreply.github.com>
2022-03-30 21:04:25 -0600

    Update home.php

--- fuel/app_project/views/m1/home.php
+++ fuel/app_project/views/m1/home.php
@@ -1,5 +1,15 @@
 <?php echo Asset::css('home.css'); ?>
 
 <div class="home">
-    <?php echo Asset::img('background.jpg'); ?>
+		<h1 class='welcome'>Welcome!</h1>
+		<p>We are Calico, a website that provides color palettes for download. To use our service navigate to the Color Coordinator page and try it out for free! To find out more about the creators navigate to about.</p>
+		<h2>How to use our Color Coordinator</h2>
+		<h3>Step 1: Navigate to Color Cooordinator</h3>
+		<?php echo Asset::img('step0.png'); ?>
+		<h3>Step 2: Select the size of your table</h3>
+		<?php echo Asset::img('step1.png'); ?>
+		<h3>Step 3: Choose your colors!</h3>
+		<?php echo Asset::img('step2.png'); ?>
+		<h3>Step 4: Print your results</h3>
+		<?php echo Asset::img('step3.png'); ?>
 </div>
