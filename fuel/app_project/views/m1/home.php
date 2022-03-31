0d4623dcbce77f846da377d65333c760223ecd2e
w45272 <w45272@rams.colostate.edu>
2022-03-30 20:37:51 -0600

    modify homepage

--- fuel/app_project/views/m1/home.php
+++ fuel/app_project/views/m1/home.php
@@ -1,6 +1,5 @@
 <?php echo Asset::css('home.css'); ?>
 
 <div class="home">
-		<h1 class='welcome'>Welcome</h1>
-		<p>We are Calico, a website that provides color palettes for download. To use our service navigate to the Color Coordinator 		page and try it out for free! To find out more about the creators navigate to about.
+    <?php echo Asset::img('background.jpg'); ?>
 </div>
