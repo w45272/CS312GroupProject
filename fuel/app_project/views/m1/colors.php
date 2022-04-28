8720ce97d77f7b74513a527b008554246128860e
backlira <backlira@rams.colostate.edu>
2022-04-28 08:18:07 -0600

    added logic

--- fuel/app_project/views/m1/colors.php
+++ fuel/app_project/views/m1/colors.php
@@ -11,8 +11,10 @@
         <?php
         for($x=0; $x<$count; $x++){
             echo "<tr><td>
-             
-            <select id='color_select".$x."' name='colors' class='color_select'> 
+            <input type='radio' id='radio".$x."' value='".$x."' name='colorChoice'";
+            if($x==0) echo "checked";
+            echo " >
+            <select id='color_select".$x."' name='colors' class='color_select' > 
                             <option value='red'>Red</option>
                             <option value='orange'>Orange</option>
                             <option value='yellow'>Yellow</option>
@@ -24,15 +26,19 @@
                             <option value='black'>Black</option>
                             <option value='teal'>Teal</option>
                            </select>
-                            
-                    </td>
-                    <td> </td>
+              <svg width='40' height='25'><rect width='40' height='25' id='cBox".$x."'></svg>        
+                    </td> 
+                    <td><p id='color_list".$x."'></p>  </td>
                   </tr>";
-            // JS to ensure unique initial selections fromo dropdown      
+            // JS to ensure unique initial selections from dropdown
+            // and add event listeners      
             echo "<script>";
             echo "document.getElementById('color_select".$x."').getElementsByTagName('option')[".$x."].selected = 'selected';";
+            echo "document.getElementById('cBox".$x."').style.fill = document.getElementById('color_select".$x."').value;";
             echo "document.getElementById('color_select".$x."').addEventListener('focus', saveValue);";
             echo "document.getElementById('color_select".$x."').addEventListener('change', validateChange);";
+            echo "document.getElementById('radio".$x."').addEventListener('change', setChosenColor);";
+            echo "var temp".$x."=[]; colorLists.push(temp".$x.");";
             echo "</script>";
         }
         ?>
@@ -49,14 +55,28 @@
         <?php
         for($row=0; $row<$size+1; $row++){
             echo "<tr>";
-            for($col=0, $letter='A'; $col<$size+1; $col++){
-                if($row==0 && $col==0){echo "<td> </td>";}
-                elseif($row==0){echo "<td> $letter </td>"; $letter++;}
+            for($col=0, $letter='A', $letter2='A'; $col<$size+1; $col++){
+                if($row===0 && $col===0){echo "<td> </td>";}
+                elseif($row==0){echo "<td> ".$letter2." </td>"; $letter2++;}
                 elseif($col==0){echo "<td> $row </td>";}
-                else{echo "<td> </td>";}
+                else{echo "<td class='none' id=".$letter.$row."> </td>"; $letter++; }
+                
             }
+            
             echo "</tr>";
         }
+        echo "<script>";
+        for($row=0; $row<$size+1; $row++){
+            for($col=0, $letter='A'; $col<$size+1; $col++){
+                if($row==0 && $col==0){;}
+                elseif($row==0){;}
+                elseif($col==0){;}
+                else{echo "document.getElementById('".$letter.$row."').addEventListener('click', tableClick);"; $letter++;}
+                
+            }
+        }
+        echo "</script>";
+        
         ?>
     
     </table>
--- local_html/m1/assets/js/colors.js
+++ local_html/m1/assets/js/colors.js
@@ -1,8 +1,18 @@
 
-        var currentSelected=[];
-        var prevSelected = "";
+        var currentSelected=[]; //current list of colors selected by dropdowns
+        var prevSelected = "";  //holder for old color when checking if new color is already selected
+        var selectedColor = ""; //Chosen color from radio buttons
+        var selectedList = 0;
+        var colorLists = [];
         
+        
+        //document ready handler
+        document.addEventListener('DOMContentLoaded', (e)=>{
+            selectedColor = document.getElementById("color_select0").value;
 
+        });
+        
+        //helper function: updates the dropdown selected colors 
         function saveValue(e){
             prevSelected = e.target.value;
             
@@ -15,11 +25,42 @@
             for (const sel of allSel){
                 currentSelected.push(sel.value);
             }
-            //console.log("array:" +currentSelected);
-            
+            var box = "cBox" + e.target.id.substring(12);
+            document.getElementById(box).style.fill = e.target.value;   
+            updateColor(e.target.id.substring(12));
         }
-        function validateChange(e){
+        //handler: set background color to selected on click
+        function tableClick(e){
+            //document.getElementById(e.target.id).className=selectedColor;
+            e.target.className=selectedColor;
+            addToTableList(e);
+        }
+        function addToTableList(e){
+            if(colorLists[selectedList].includes(e.target.id)) return;
+            for(const list of colorLists){
+                if(list.includes(e.target.id)){
+                    for(var i=0;i<list.length; i++){
+                        if(list[i] === e.target.id){
+                            list.splice(i,1);
+                        }
+                    }
+                }
+            }            
+            colorLists[selectedList].push(e.target.id);
+            //console.log(colorLists[selectedList].toString());
+
+            updateLists();            
+        }
+        function updateLists(){ 
+            //console.log("lists: "+colorLists.length);          
+            for(var i=0; i<colorLists.length; i++){
+                document.getElementById("color_list"+i).innerText = colorLists[i].toString();
+            }
+        }
         
+        //helper function: check if color is already in list when picking new color
+        function validateChange(e){
+                                    
             let x = document.getElementById('warning_msg');
             if(x != null){x.remove();}
                     
@@ -29,5 +70,15 @@
                 
                 }
             else{saveValue(e);}
-        }        
+        }
+           function setChosenColor(e){
+                selectedColor = document.getElementById("color_select"+e.target.value).value; 
+                selectedList = e.target.value;
+                               
+           }
+           function updateColor(e){
+                for(const id of colorLists[e]){
+                    document.getElementById(id).className = selectedColor;
+                } 
+           }        
 
